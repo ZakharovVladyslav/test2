29fb512fb25da4aa0d7e45590b16f5e2aeaec5bb
ZakharovVladyslav <vladislavozakharov@gmail.com>
2024-05-25 14:25:38 +0300

    апд: додав search компонент; фікс: додав todo лісту скрол якщо висота контенту більша за висоту контейнеру; фікс: розтягнув контейнери на сторінці на всю висоту (side panel та content)

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,19 @@
-import './Header.scss';
+import { Search } from "../Search/Search";
+import "./Header.scss";
 
 const MainHeader = () => {
-    return (
-        <div>
-            <div className="header">
-                <div className="header_text">
-                    <div className="header_descr">Monday</div>
-                    <div className="header_subdescr">13rd, September 2024</div>
-                </div>
-                <input className="header_search" type="seacrh" placeholder='Seacrh'/>
+   return (
+      <div>
+         <div className="header">
+            <div className="header_text">
+               <div className="header_descr">Monday</div>
+               <div className="header_subdescr">13rd, September 2024</div>
             </div>
-        </div>
-    )
-
-}
+            {/* <input className="header_search" type="seacrh" placeholder='Seacrh'/> */}
+            <Search />
+         </div>
+      </div>
+   );
+};
 
 export default MainHeader;
